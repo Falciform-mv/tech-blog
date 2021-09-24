d790ec4057cdc09b44ea6d1e17b3683ddf9265bd
Mike Veneziale <falciformdesign@gmail.com>
2021-09-23 19:17:43 -0500

    added user model

---
+++ models/User.js
@@ -0,0 +1,56 @@
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// creates user model
+class User extends Model {}
+
+// defines table columns and configurations
+User.init(
+  {
+    // defines id column
+    id: {
+      // uses Sequelize DataTypes object to provide what type of data it is
+      type: DataTypes.INTEGER,
+      // SQL's not null option
+      allowNull: false,
+      // instructs primary key
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // defines username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // defines email column
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // cannot be duplicate email values in table
+      unique: true,
+      // if allownull is set to false we can run our data through validators
+      validate: {
+        isEmail: true
+      }
+    },
+    // defines password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // password must be at least four characters long
+        len: [4]
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+model.exports = User;
