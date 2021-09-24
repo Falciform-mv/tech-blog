8f103d358697a27e9a43aeec9afb65f856110e9e
Mike Veneziale <falciformdesign@gmail.com>
2021-09-23 19:31:12 -0500

    updated user model and exported it

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+// constructor function to accept database info 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
