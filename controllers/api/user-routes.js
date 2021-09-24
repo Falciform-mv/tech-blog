236c4177be1476b0a8ffef5bd125f5f2deb18c40
Mike Veneziale <falciformdesign@gmail.com>
2021-09-23 20:32:02 -0500

    connected routes and started server

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,3 +3,91 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // GET api users
+router.get('/', (req, res) => {
+  // access our user model and run findall method
+  User.findAll()
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// GET api/users/1
+router.get('/:id', (req, res) => {
+  User.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'no user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// POST api/users
+router.post('/', (req, res) => {
+  // inserts data into users table username, email, password
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// PUT api/users/1
+router.put('/:id', (req, res) => {
+  // if reqbody has exact pairs to match the model, you can use just req body
+  // SQL Syntax would say update users set username where id = 1
+  User.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData[0]) {
+      res.status(404).json({ message: 'no user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+// DELETE a;i/users/1
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'no user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+module.exports = router;
