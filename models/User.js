69418fd6e0fba9523246788f7c2f0e219fc9db0e
Mike Veneziale <falciformdesign@gmail.com>
2021-09-26 19:36:11 -0500

    fixed tables

--- models/User.js
+++ models/User.js
@@ -1,69 +1,57 @@
-
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-// creates user model
+// create our User model
 class User extends Model {
-  // set up method to run on instance data to check password
+  // set up method to run on instance data (per user) to check password
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-// defines table columns and configurations
+// create fields/columns for User model
 User.init(
   {
-    // defines id column
     id: {
-      // uses Sequelize DataTypes object to provide what type of data it is
       type: DataTypes.INTEGER,
-      // SQL's not null option
       allowNull: false,
-      // instructs primary key
       primaryKey: true,
       autoIncrement: true
     },
-    // defines username column
     username: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    // defines email column
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      // cannot be duplicate email values in table
       unique: true,
-      // if allownull is set to false we can run our data through validators
       validate: {
         isEmail: true
       }
     },
-    // defines password column
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        // password must be at least four characters long
         len: [4]
       }
     }
   },
   {
     hooks: {
-      // sets up beforeCreate lifecycle hook functionality
+      // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
-        },
+      },
 
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
       }
-
-      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
